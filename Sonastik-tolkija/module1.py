17586ca43523f10b8269b6bf7fc4237e6065caa2
Ervin Liiv <77278208+zzzervin@users.noreply.github.com>
2022-01-20 14:01:30 +0200

    Update module1.py

--- Sonastik-tolkija/module1.py
+++ Sonastik-tolkija/module1.py
@@ -13,3 +13,16 @@ def loe_failist(f:str,l:list):
     fail.close()
     return mas
 
+def uus_sona(f:str,rida:str,l:list)->list:
+    l=[]
+    with open(f,"a",encoding="utf-8-sig") as fail:
+        fail.write(rida+"\n")
+    l=failist_lugemine(f)
+    return l
+            
+#---------------------------------------------------------------
+import os
+from gtts import gTTS
+def Heli(text:str,keel:str):
+    obj=gTTS(text=text,lang=keel,slow=False).save("heli.mp3")
+    os.system("heli.mp3")
